b380c7f6d165f3f2c992c3176a00211492c6eb02
Illuminati-jpg <ogelato37@gmail.com>
2025-08-04 02:49:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.get('/', (req, res) => res.send('Updated at 03:34 AM!'));
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(` Server running at https://localhost:${PORT}`));
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
 
 app.use(cors());
 app.use(express.json());
